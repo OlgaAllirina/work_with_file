ba5d8885d8c2b40537b541047b26a870f40b46cf
Olga Menuhova <wowolgaivanova@gmail.com>
2023-11-03 10:30:47 +0300

    add filies and test2

--- oopmain.py
+++ oopmain.py
@@ -1,24 +1,6 @@
 from collections import defaultdict
 
-"""cook_book = {
-  'Омлет': [
-    {'ingredient_name': 'Яйцо', 'quantity': 2, 'measure': 'шт.'},
-    {'ingredient_name': 'Молоко', 'quantity': 100, 'measure': 'мл'},
-    {'ingredient_name': 'Помидор', 'quantity': 2, 'measure': 'шт'}
-    ],
-  'Утка по-пекински': [
-    {'ingredient_name': 'Утка', 'quantity': 1, 'measure': 'шт'},
-    {'ingredient_name': 'Вода', 'quantity': 2, 'measure': 'л'},
-    {'ingredient_name': 'Мед', 'quantity': 3, 'measure': 'ст.л'},
-    {'ingredient_name': 'Соевый соус', 'quantity': 60, 'measure': 'мл'}
-    ],
-  'Запеченный картофель': [
-    {'ingredient_name': 'Картофель', 'quantity': 1, 'measure': 'кг'},
-    {'ingredient_name': 'Чеснок', 'quantity': 3, 'measure': 'зубч'},
-    {'ingredient_name': 'Сыр гауда', 'quantity': 100, 'measure': 'г'},
-    ]
-  }
-"""
+# функция для создания словаря cook_book с соответствующими ключами и значениями
 
 def tuple_text_book(text_book, cook_book):
   for string in text_book:
@@ -27,13 +9,35 @@ def tuple_text_book(text_book, cook_book):
         dish = string.rstrip()
         for _ in range(int(text_book.readline().rstrip())):
           dish_ingredients.append(dict(zip(('ingredient_name', 'quantity', 'measure'), text_book.readline().rstrip().split(' | '))))
-          cook_book[dish] = dish_ingredients                
+          cook_book[dish] = dish_ingredients          
+  
 
+# функция для определения количества ингридентов на указанное количество гостей
+
+def get_shop_list_by_dishes(dishes, person_count):
+   dishes_dict = defaultdict(dict)
+   for dish in dishes:
+      for product in cook_book[dish]:
+         dishes_name = product['ingredient_name']
+         if dishes_name in dishes_dict:
+            dishes_dict[dishes_name]['quantity'] += int(product['quantity'])*person_count
+         else:
+            dishes_dict[dishes_name]['measure'] = product['measure']
+            dishes_dict[dishes_name]['quantity'] = int(product['quantity'])*person_count
+   return dishes_dict
+      
+
+# открываем файл для чтпения информации с кодировкой utf-8 и присваиваем ему переменную text_book
 
 with open("oop.txt", "r", encoding="utf-8") as text_book:
     cook_book = {}
+    dishes = {}
+    person_count = 4
     print(tuple_text_book(text_book, cook_book))
     
+   
+
+# цикл для корректного отображения словаря cook_book
 
 for key in cook_book.keys():
     print(f"{key}:")
@@ -41,8 +45,13 @@ for key in cook_book.keys():
         print(value)
 print()
             
+# создадим лист блюд на указанное количество персон  
+person = 4
+dishes_list = get_shop_list_by_dishes(["Запеченный картофель", "Фахитос"], person)
+#print(f'Список ингридиентов для блюд: Фахитос, Запеченный картофель на {person} человек:', end='\n\n')
+for key, value in dishes_list.items():
+    print(f"{key}: {value}")
     
-
 
 
         
